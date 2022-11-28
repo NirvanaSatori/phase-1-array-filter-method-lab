01199d74fd1e8c7088567b2d2016b684d5db59ff
MORRIS <morris24kariuki@gmail.com>
2022-11-28 16:53:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(array, driver){
+    return array.filter(function (name){
+      return (name.toUpperCase() === driver.toUpperCase())
+    })
+}
+
+
+function fuzzyMatch(array, driver) {
+    return array.filter (name => name.slice(0,2) ===driver)
+}
+
+function matchName(array, driver){
+    return array.filter(structure =>structure.name ===driver
+    )
+}
